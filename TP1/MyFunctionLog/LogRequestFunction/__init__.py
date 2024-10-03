b9a4b5083d62d39d919b9ab78f862658ff088ebc
Damien <77857194+dada919@users.noreply.github.com>
2024-10-03 13:59:08 +0200

    update readme

--- TP1/MyFunctionLog/LogRequestFunction/__init__.py
+++ TP1/MyFunctionLog/LogRequestFunction/__init__.py
@@ -2,7 +2,7 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.function_name(name="LogRequestFunction")  # Ceci doit correspondre au nom de votre fonction
+@app.function_name(name="LogRequestFunction")
 @app.route(route="logrequest", auth_level=func.AuthLevel.ANONYMOUS)
 def log_request(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(f"Request received: {req.get_body()}")
--- TP1/readme.md
+++ TP1/readme.md
@@ -3,3 +3,5 @@
 ![Azure Functions](./1.PNG)
 
 https://tp1damien.azurewebsites.net/api/helloworldfunction
+
+![Azure Functions](./2.PNG)
